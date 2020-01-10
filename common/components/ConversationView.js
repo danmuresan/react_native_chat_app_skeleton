c059b19388e6c6d2a1231d3794035a031d03d584
danmuresan <dan91142006@yahoo.com>
2020-01-10 17:21:45 +0200

    Fix styling for message composer

--- common/components/ConversationView.js
+++ common/components/ConversationView.js
@@ -6,18 +6,40 @@ import { AppColors } from './ui-helpers/Colors'
 import getLocalizedString from './ui-helpers/strings/StringLocalizer'
 
 export default class ConversationView extends React.Component {
-    render () {
+    constructor(props) {
+        super(props);
+        this.state = {
+            messageComposerHeight: 50
+        }
+    }
+
+    componentDidMount() {
+        this.setState(this.state);
+    }
+    
+    render() {
         // TODO: ...
         return (
             <View style={CommonStyles.base}>
-                <Text style={styles.placeHolderText}>
-                    Conversation History will go here...
-                </Text>
-                <View style={styles.messageComposerContainer}>
-                    <TextInput placeholder={getLocalizedString("TypeMessageLabel")} style={styles.messageComposer} />
-                    <View style={styles.actionIcons}>
-                        <Icon size={40} marginStart={8} color={AppColors.appBrand} name='camera'/>
-                        <Icon size={40} marginStart={8} color={AppColors.appBrand} name='keyboard-voice'/>
+                <View style={styles.conversationContainer}>
+                    <Text style={styles.placeHolderText}>
+                        Conversation History will go here...
+                    </Text>
+                </View>
+                <View style={{flexDirection: 'row'}}>
+                    <View style={styles.messageComposerContainer}>
+                        <TextInput 
+                            multiline={true}
+                            numberOfLines={5}
+                            onContentSizeChange={(event) => {
+                                this.setState({ messageComposerHeight: event.nativeEvent.contentSize.height })
+                            }}
+                            placeholder={getLocalizedString("TypeMessageLabel")} 
+                            style={[styles.messageComposer, {height: Math.max(50, this.state.messageComposerHeight)}]} />
+                        <View style={styles.actionIcons}>
+                            <Icon size={40} marginStart={8} color={AppColors.appBrand} name='camera'/>
+                            <Icon size={40} marginStart={8} color={AppColors.appBrand} name='keyboard-voice'/>
+                        </View>
                     </View>
                 </View>
             </View>
@@ -27,14 +49,17 @@ export default class ConversationView extends React.Component {
 
 const styles = StyleSheet.create({
     placeHolderText: {
-        flex: 0.92, 
+        flex: 1, 
         alignItems: 'center', 
         justifyContent: 'center', 
         textAlign: 'center',
         textAlignVertical: 'center'
     },
+    conversationContainer: {
+        flex: 3
+    },
     messageComposerContainer: {
-        flex: 0.08,
+        flex: 1,
         width: '100%',
         paddingStart: 16,
         paddingEnd: 8,
@@ -44,6 +69,8 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderRadius: 40,
         borderColor: AppColors.appBrand,
+        padding: 12,
+        maxHeight: 90,
         width: '72%',
         alignItems: 'center',
         justifyContent: 'center',
